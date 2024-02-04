305d07ef98a2a364e8d09184a1c350b01c394664
Your Name <you@example.com>
2024-02-04 18:22:00 +0100

    console.php

--- routes/console.php
+++ routes/console.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Facades\Artisan;
 | This file is where you may define all of your Closure based console
 | commands. Each Closure is bound to a command instance allowing a
 | simple approach to interacting with each command's IO methods.
+
+hallo reda we gehts es dir ?
 |
 */
 
