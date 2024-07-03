0e559ee5f2d409dc7212bc9381b185cec9da6639
ELMAHDI-YAHIAOUI <yahiaouimahdi43@gmail.com>
2024-07-03 02:45:55 +0100

    last version

--- resources/views/formateur/create.blade.php
+++ resources/views/formateur/create.blade.php
@@ -1,78 +1,78 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ url('css/createFormateur.css') }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+</head>
+<body>
+
+
 <div class="container">
-    <h2>Create Formateur</h2>
-    <form action="{{ route('formateur.store') }}" method="POST">
-        @csrf
-        <div class="form-group">
-            <label for="name">nom:</label>
-            <input type="text" class="form-control" id="name" name="nom" required>
-        </div>
+    <div class="col-left">
+        <img src="{{ asset('img/Doit-on taire nos émotions au bureau _ (1).jpeg') }}" alt="" width="100%">
+    </div>
+    <div class="col-right">
 
-
-
-        <div class="form-group">
-            <label for="name">prenom:</label>
-            <input type="text" class="form-control" id="name" name="prenom" required>
-        </div>
-
-
-
-        <div class="form-group">
-            <label for="name">telephone:</label>
-            <input type="text" class="form-control" id="name" name="telephone" required>
-        </div>
-
-
-        <div class="form-group">
-            <label for="name">CIN:</label>
-            <input type="text" class="form-control" id="name" name="CIN" required>
-        </div>
-
-
-
-
-        <div class="form-group">
-            <label for="name">addresse :</label>
-            <input type="text" class="form-control" id="name" name="addresse" required>
-        </div>
-
-
-
-
-        <div class="form-group">
-            <label for="name">date de naissance  :</label>
-            <input type="date" class="form-control" id="name" name="dateNaissance" required>
-        </div>
-
-
-
-
-
-
-
-
-
-
-        <div class="form-group">
-            <label for="classes">Classes:</label>
-            <select multiple class="form-control" id="classes" name="classes[]" required
-                @foreach($classes as $classe)
-
-
-<option value="{{ $classe->id_classe }}"> {{ $classe->num_groupe }} - Niveau: {{ $classe->niveau->niveau }} -
-
-@foreach ($filieres as $item)
-@if ($item->id_filiere === $classe->niveau->id_filiere  )
-
-Filiere:{{$item->nom_filiere }}
-@endif
-@endforeach
-
-                    </option>
-                @endforeach
-            </select>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+<form class="w-md mx-auto" action="{{ route('formateur.store') }}" method="POST">
+<h1 class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Ajouter Formateur</h1>
+<div class="grid md:grid-cols-2 md:gap-6">
+  <div class="relative z-0 w-full mb-5 group">
+      <input type="text" name="nom" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+      <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nom</label>
+  </div>
+  <div class="relative z-0 w-full mb-5 group">
+      <input type="text" name="prenom" id="floating_last_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+      <label for="floating_last_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Prenom</label>
+  </div>
+</div>
+<div class="relative z-0 w-full mb-5 group">
+    <input type="tele" name="telephone" id="tele" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+    <label for="tele" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Telephone</label>
+</div>
+<div class="relative z-0 w-full mb-5 group">
+    <input type="text" name="CIN" id="cin" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+    <label for="cin" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">CIN</label>
 </div>
 
+<div class="relative z-0 w-full mb-5 group">
+    <input type="adress" name="addresse" id="adress" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+    <label for="adress" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Adresse</label>
+</div>
+<div class="relative z-0 w-full mb-5 group">
+    <input type="date" name="dateNaissance" id="date" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+    <label for="date" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Date De Naissance</label>
+</div>
+
+<label for="countries_multiple" class="block mb-2 text-sm font-medium text-gray-500 dark:text-gray-400">Select Countries</label>
+<select multiple id="countries_multiple"  name="classes[]" required class="bg-gray-10 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    @foreach($classes as $classe)
+
+
+    <option value="{{ $classe->id_classe }}"> {{ $classe->num_groupe }} - Niveau: {{ $classe->niveau->niveau }} -
+
+    @foreach ($filieres as $item)
+    @if ($item->id_filiere === $classe->niveau->id_filiere  )
+
+    Filiere:{{$item->nom_filiere }}
+    @endif
+    @endforeach
+
+                        </option>
+                    @endforeach
+</select><br>
+
+
+<button>Submit <span>⟶</span></button>  </form>
+
+    </div>
+
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+
+</body>
+</html>
